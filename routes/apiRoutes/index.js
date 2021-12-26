bb9d3aeddc846bbd3e491f85a5a69a9c6b1fd6c4
Katelyn Bunker <kerbunker@gmail.com>
2021-12-26 15:14:24 -0500

    Add routes for saving and getting notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
-//const { notes } = require('../../db/db');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+let notes = require('../../db/db');
+const path = require('path');
+
+const createNewNote = (body, notesArray) => {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return note;
+};
 
 router.get('/notes', (req, res) => {
     let results = notes;
+
+    res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4();
     
-})
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+module.exports = router;
